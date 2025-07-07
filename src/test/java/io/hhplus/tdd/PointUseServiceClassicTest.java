5bbb55cbd2f057ece037cec692738f242e2b7e4a
SUNGIN99 <99_insung@naver.com>
2025-07-08 00:41:46 +0900

    TDD 작성 4 (Classic-고전파 테스트 코드 작성) - 실제 객체사용 - 실제 객체를 사용하되, 내부 로직 행위는 정확하게 알지못한다는 가정을 한채로 블랙박스 테스트를 지향해보려고 노력해보기..

---
+++ src/test/java/io/hhplus/tdd/PointUseServiceClassicTest.java
@@ -0,0 +1,118 @@
+package io.hhplus.tdd;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.PointService;
+import io.hhplus.tdd.point.TransactionType;
+import io.hhplus.tdd.point.UserPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PointUseServiceClassicTest {
+
+    private UserPointTable userPointTable;
+    private PointHistoryTable pointHistoryTable;
+    private PointService pointService;
+
+    @BeforeEach
+    void setUp(){
+        userPointTable = new UserPointTable();
+        pointHistoryTable = new PointHistoryTable();
+        pointService = new PointService(pointHistoryTable, userPointTable);
+    }
+
+    @Test
+    void T1_WHITE_포인트_충전내역_기본조회(){
+        // given
+        Long id = 1L;
+        Long currentTimeMillis =  System.currentTimeMillis();
+        UserPoint newUser = pointService.getPoint(id);
+
+        assertThat(newUser).isNotNull();
+        assertThat(newUser.point()).isEqualTo(0L);
+        assertThat(newUser.updateMillis()).isGreaterThanOrEqualTo(currentTimeMillis);
+
+        currentTimeMillis = System.currentTimeMillis();
+        UserPoint oldUser = pointService.getPoint(id);
+
+        assertThat(oldUser).isNotNull();
+        assertThat(oldUser.updateMillis()).isLessThan(currentTimeMillis);
+    }
+
+    @Test
+    void T1_BLACK_포인트_충전내역_기본조회(){
+        Long id = 1L;
+        long chargeAmount = 10L;
+
+        // 신규 유저 조회
+        UserPoint newUser = userPointTable.selectById(id);
+        assertThat(newUser.point()).isEqualTo(0L);
+
+        // 신규 유저 충전 -> 기존 유저로 전환
+        UserPoint chargeUser = pointService.charge(id, chargeAmount);
+
+        // 신규, 기존 유저 조회 기준이 updateTimeMillis 인지 모르고 진행.
+        // 포인트 충전도 동시에 테스트로 검증
+        UserPoint userPoint = pointService.getPoint(id);
+        assertThat(userPoint).isNotNull();
+        assertThat(userPoint.id()).isEqualTo(id);
+        assertThat(userPoint.point()).isEqualTo(chargeAmount);
+    }
+
+
+    @Test
+    void T2_포인트가_충분하면_사용한다(){
+        Long userId = 1L;
+        long curAmount = 20L;
+        long useAmount = 10L;
+
+        // 사전 준비,,
+        userPointTable.insertOrUpdate(1L, curAmount);
+        pointHistoryTable.insert(userId, curAmount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        // when
+        UserPoint userPoint = pointService.use(userId, useAmount);
+        assertThat(userPoint).isNotNull();
+        assertThat(userPoint.id()).isEqualTo(userId);
+        assertThat(userPoint.point()).isEqualTo(curAmount - useAmount);
+
+        UserPoint stored = userPointTable.selectById(userId);
+        assertThat(stored.point()).isEqualTo(curAmount - useAmount);
+
+        List<PointHistory> histories = pointHistoryTable.selectAllByUserId(userId);
+        assertThat(histories).asList().hasSize(2); // 첫 충전 내역 + 사용 내역
+        assertThat(histories.get(1).amount()).isEqualTo(useAmount);
+        assertThat(histories.get(1).type()).isEqualTo(TransactionType.USE);
+    }
+
+    @Test
+    void T2_포인트가_0이거나_부족하다면_사용할_수_없다(){
+        Long userId = 1L;
+        long curAmount = 20L;
+        long useAmount = 10L;
+
+        // 사전 준비,,
+        userPointTable.insertOrUpdate(1L, curAmount);
+        pointHistoryTable.insert(userId, curAmount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        // when
+        UserPoint userPoint = pointService.use(userId, useAmount);
+        assertThat(userPoint).isNotNull();
+        assertThat(userPoint.id()).isEqualTo(userId);
+        assertThat(userPoint.point()).isEqualTo(curAmount - useAmount);
+
+        UserPoint stored = userPointTable.selectById(userId);
+        assertThat(stored.point()).isEqualTo(curAmount - useAmount);
+
+        List<PointHistory> histories = pointHistoryTable.selectAllByUserId(userId);
+        assertThat(histories).asList().hasSize(2); // 첫 충전 내역 + 사용 내역
+        assertThat(histories.get(1).amount()).isEqualTo(useAmount);
+        assertThat(histories.get(1).type()).isEqualTo(TransactionType.USE);
+    }
+
+}
